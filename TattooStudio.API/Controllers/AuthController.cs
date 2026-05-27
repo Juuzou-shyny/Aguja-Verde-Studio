a438a427ae9e5e16316fe97cbabed29192975fff
Juuzou-shyny <118845580+Juuzou-shyny@users.noreply.github.com>
2026-05-27 21:05:53 +0200

    correcion

--- TattooStudio.API/Controllers/AuthController.cs
+++ TattooStudio.API/Controllers/AuthController.cs
@@ -21,12 +21,6 @@ public class AuthController : ControllerBase
         _config = config;
     }
 
-    [HttpGet("hash/{password}")]
-    public IActionResult GetHash(string password)
-    {
-        return Ok(BCrypt.Net.BCrypt.HashPassword(password));
-    }
-
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginRequest request)
     {
